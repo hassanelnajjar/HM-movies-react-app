b3aad395afbe6dbf15ccf5ef029d712cb90422c6
Muhammad Alabadsa <moh22389@gmail.com>
2021-01-13 17:48:01 +0200

    fix duplicate files and some style

--- src/components/MoviesList/index.js
+++ src/components/MoviesList/index.js
@@ -8,27 +8,32 @@ function MoviesList(props) {
   return (
     <>
       <ul>
-        {movies ? movies.map((movie) => (
-          <li
-            tabIndex="0"
-            role="button"
-            aria-label="select-movie"
-            key={movie.id}
-            className="AddMovie-movie-item"
-            onClick={(e) => handleChooseMovie(movie.id, e)}
-            onKeyDown={(e) => handleChooseMovieByKey(movie.id, e)}
-          >
-            <p className="movie-original-title">{movie.original_title}</p>
-            <img
-              className="movie-thumbnail"
-              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-              alt="movie poster"
-            />
-            <span className="movie-release-year">
-              {new Date(movie.release_date).getFullYear()}
-            </span>
-          </li>
-        )) : null}
+        {movies
+          ? movies.map((movie) => (
+            <li
+              tabIndex="0"
+              role="button"
+              aria-label="select-movie"
+              key={movie.id}
+              className="AddMovie-movie-item"
+              onClick={(e) => handleChooseMovie(movie.id, e)}
+              onKeyDown={(e) => handleChooseMovieByKey(movie.id, e)}
+            >
+              <div className="movie-original-title-container">
+                <p className="movie-original-title">{movie.original_title}</p>
+                <span className="movie-release-year">
+                  Release on
+                  {new Date(movie.release_date).getFullYear()}
+                </span>
+              </div>
+              <img
+                className="movie-thumbnail"
+                src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                alt="movie poster"
+              />
+            </li>
+            ))
+          : null}
       </ul>
     </>
   );
